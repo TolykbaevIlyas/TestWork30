24837dd3464eb45fb4246e88750324994270e4bc
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2025-04-17 21:15:13 +0500

    feat: create weatherapi

---
+++ src/shared/api/weatherApi.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
+const baseUrl = "https://api.openweathermap.org/data/2.5";
+
+const instance = axios.create({ baseURL: baseUrl });
+
+export const weatherApi = {
+  getCurrentWeather: (city: string) =>
+    instance.get(`/weather?q=${city}&appid=${apiKey}&units=metric`),
+
+  getForecast: (city: string) =>
+    instance.get(`/forecast?q=${city}&appid=${apiKey}&units=metric`)
+};
