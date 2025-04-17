f1f0c545437f507cf5906d1f426d457625e555fd
TolykbaevIlyas <77828905+TolykbaevIlyas@users.noreply.github.com>
2025-04-17 21:51:17 +0500

    feat: create favorites page

---
+++ src/app/(pages)/favorites/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useWeatherStore } from "@/shared/model/weatherStore";
+import { WeatherCard } from "@/entities/weather/ui/WeatherCard";
+
+const FavoritesPage = () => {
+  const { favorites } = useWeatherStore();
+
+  return (
+    <div className="container py-4">
+      <h1 className="mb-4">Избранные города</h1>
+
+      {favorites.length === 0 ? (
+        <p>Добавьте города в избранное, чтобы они здесь появились.</p>
+      ) : (
+        <div className="row">
+          {favorites.map((city, index) => (
+            <div key={index} className="col-md-2 mb-4">
+              <WeatherCard city={city} temperature={0} description="" />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FavoritesPage;
